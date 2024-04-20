edc421fbf046cd11bcf789dfc0bef653778835a4
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2024-04-20 16:33:30 +0530

    Inital Commit

--- app/(pages)/home/page.js
+++ app/(pages)/home/page.js
@@ -1,7 +1,13 @@
 import { Wrapper } from '../(components)/wrapper'
 import s from './home.module.scss'
 
+import { useLenis } from '@studio-freight/react-lenis'
+
 export default function Home() {
+  useLenis(({ scroll }) => {
+    console.log(scroll)
+  })
+
   return (
     <Wrapper theme="red" className={s.page}>
       {/* content  */}
